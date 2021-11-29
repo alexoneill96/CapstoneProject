cb70604052e7ec85b44f850bc144ca2517650f89
alexoneill96 <alexander-oneill@hotmail.com>
2021-11-29 15:27:54 +0000

    Code quality check - ErrorMessages.js

--- application-front-end/src/components/Create/ErrorMessages.js
+++ application-front-end/src/components/Create/ErrorMessages.js
@@ -1,12 +1,12 @@
-// Field error messages
+// FORM ERROR MESSAGES
 export const PREFIX_REQUIRED = "Prefix required";
 export const FIRST_NAME_REQUIRED = "First name required";
 export const LAST_NAME_REQUIRED = "Last name required";
 export const TELEPHONE_NUMBER_REQUIRED = "Telephone number required";
-export const TELEPHONE_NUMBER_INVALID = "Telephone number INVALID";
+export const TELEPHONE_NUMBER_INVALID = "Telephone number invalid";
 export const ADDRESS_LINE_1_REQUIRED = "Address Line 1 required";
 export const CITY_REQUIRED = "City required";
-export const ZIPCODE_REQUIRED = "Postcode required";
+export const ZIPCODE_REQUIRED = "Zipcode required";
 export const VEHICLE_TYPE_REQUIRED = "Vehicle Type required";
 export const ENGINE_SIZE_REQUIRED = "Engine size required";
 export const ADDITIONAL_DRIVERS_REQUIRED = "Additional drivers required";
