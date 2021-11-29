9df2a3b64ee9eaa5eb3c850e2faa5f36bd4f30b0
alexoneill96 <alexander-oneill@hotmail.com>
2021-11-29 15:42:35 +0000

    Code quality check - ApplicantService.java

--- src/main/java/com/example/applicants/service/ApplicantService.java
+++ src/main/java/com/example/applicants/service/ApplicantService.java
@@ -1,17 +1,16 @@
 package com.example.applicants.service;
 
+// IMPORTS
 import com.example.applicants.model.Applicant;
 import com.example.applicants.repository.ApplicantRepository;
 import com.example.applicants.service.businessLogic.QuoteAmountCalculator;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
 public class ApplicantService {
-
     private final ApplicantRepository repository;
     private final QuoteAmountCalculator quoteAmountCalculator;
     List<Applicant> applicantList = new ArrayList<>();
@@ -25,36 +24,33 @@ public class ApplicantService {
         return repository.findAll();
     }
 
+    // SAVE NEW RECORD
     public Applicant save(Applicant applicant) {
         quoteAmountCalculator.calculateQuote(applicant);
         applicantList.add(applicant);
-        // DisplayDriverDetails.displayDriverDetails(driverList);
-
         return repository.save(applicant);
-    }//Save a New Record
+    }
 
 
-
+    // GET A SINGLE APPLICANT
     public Applicant getSingleApplicant(Long id) {
-
-
         return repository.findById(id).orElseThrow(()-> new NoSuchElementException("Driver with ID "+id+" not found."));
-    } //GET a single applicant
+    }
 
 
+    // DELETE SINGLE APPLICANT
     public void deleteSingleApplicant(Long id) {
         if(repository.findById(id)!=null){
             repository.deleteById(id);
         } else throw new NoSuchElementException("Driver with ID"+id+" not found.");
+    }
 
-    } //DELETE
-
+    // UPDATE APPLICANT TELEPHONE NUMBER
     public Applicant updateNumber(Long id, String telephoneNumber) {
-
         return repository.findById(id)
                 .map(recordForUpdating -> {
                     recordForUpdating.setTelephoneNumber(telephoneNumber);
                     return repository.save(recordForUpdating);
                 }).orElseThrow(()-> new NoSuchElementException("Driver with ID "+id+" not found."));
-    }//Update Number
+    }
 }
